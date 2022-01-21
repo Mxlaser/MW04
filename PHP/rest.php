f80a70674f1705795bdf34d9ce6b1b6c4d400a4b
nzivkovic <nzivkovic@larmand.fr>
2022-01-21 15:29:22 +0100

    MW04 fin

--- PHP/rest.php
+++ PHP/rest.php
@@ -8,18 +8,26 @@
   print_r($reponse);
   $reqpreparer->closeCursor();*/
 
-  if (isset($_GET['validForm'])){
-    $pseudo=$_GET['pseudo'];
-    $mdp=$_GET['mdp'];
-    /*echo "le pseudo vaut ".$pseudo;
-    echo "le mdp vaut ".$mdp;*/
-    $mydb=new PDO('mysql:host=localhost;dbname=MW04_drone_nikola;charset=utf8','nikola','snirlla');
-    $req="select * from utilisateur where pseudo=$pseudo";
-    $reqpreparer=$mydb->prepare($req);
-    $tableauDeDonnees=array();
-    $reqpreparer->execute($tableauDeDonnees);
-    $reponse=$reqpreparer->fetchAll(PDO::FETCH_ASSOC);
-    print_r($reponse);
-    $reqpreparer->closeCursor();
+  if (isset($_POST['valider'])){
+    $pseudo=$_POST['pseudo_Utilisateur'];
+    $mdp=$_POST['mot_De_Passe_Utilisateur'];
   }
+
+  $mydb=new PDO('mysql:host=localhost;dbname=MW04_drone_nikola;charset=utf8','nikola','snirlla');
+  $req="select nom,prenom from utilisateur where pseudo='$pseudo' and mdp='$mdp'";
+  $reqpreparer=$mydb->prepare($req);
+  $tableauDeDonnees=array();
+  $reqpreparer->execute($tableauDeDonnees);
+  $reponse=$reqpreparer->fetchAll(PDO::FETCH_ASSOC);
+  $reponse2=count($reponse);
+  if($reponse2<1)
+  {
+    echo "Cet utilisateur n'existe pas !";
+  }
+  else {
+    print_r($reponse);
+  }
+  //echo "Le pseudo vaut ".$pseudo;
+  //echo " Le mdp vaut ".$mdp;
+  $reqpreparer->closeCursor();
 ?>
